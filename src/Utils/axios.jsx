4a66fc0175ce5375d7d26aa1a5c6240406d25071
Suraj Rawat <rawatsuraj9002@gmail.com>
2024-12-07 17:27:16 +0530

    api Updated

--- src/Utils/axios.jsx
+++ src/Utils/axios.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 const apiKey=import.meta.env.VITE_API_KEY;
+console.log(apiKey)
 const instance=axios.create({
     baseURL: "https://api.themoviedb.org/3/",
     headers: {
         accept: 'application/json',
-        Authorization: apiKey,
+        Authorization:apiKey,
       }
 })
 export default instance;
